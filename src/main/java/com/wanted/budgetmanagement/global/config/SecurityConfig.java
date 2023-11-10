89d0f5b23c15ba913ccf7efcab9577aa1e91defc
parkjungmin <aossuper7@naver.com>
2023-11-10 17:15:36 +0900

    feat: cors, jwtSecurity 설정 #1

---
+++ src/main/java/com/wanted/budgetmanagement/global/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.wanted.budgetmanagement.global.config;
+
+import com.wanted.budgetmanagement.global.jwt.JwtSecurityConfig;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig {
+    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+    private final CorsFilter corsFilter;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .addFilterBefore(corsFilter, UsernamePasswordAuthenticationFilter.class)
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authorizeHttpRequests(request -> request
+                        .requestMatchers("/budget/**", "/expenses/**", "/members/**").authenticated()
+                        .anyRequest().permitAll()
+                )
+                .apply(new JwtSecurityConfig(jwtTokenProvider));
+        return http.build();
+    }
+}
